f2ef7f5c662ab199a0d0377a1f01c02d8fc407c8
Miguel Brown <browncoloma@gmail.com>
2022-10-03 23:54:24 -0300

    barra navegacion completa

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const menu = document.querySelector('.hamburguesa');
+const navegacion = document.querySelector('.navegacion');
+
+document.addEventListener('DOMContentLoaded',()=>{
+    evento();
+});
+
+const evento = () => {
+    menu.addEventListener('click', abrirMenu);
+}
+
+const abrirMenu = () => {
+    navegacion.classList.remove('ocultar');
+    botonCerrar();
+}
+
+const botonCerrar = () => {
+    const btnCerrar = document.createElement('p');
+    const overlay = document.createElement('div');
+    overlay.classList.add('pantalla-completa');
+    const body = document.querySelector('body');
+    if(document.querySelectorAll('.pantalla-completa').length>0)return;
+    body.appendChild(overlay);
+    btnCerrar.textContent = 'X';
+    btnCerrar.classList.add('btn-cerrar');
+    // while(navegacion.children[4]){
+    //     navegacion.removeChild(navegacion.children[4]);
+    // }
+    navegacion.appendChild(btnCerrar);
+    cerrarMenu(btnCerrar, overlay);
+}
+
+const cerrarMenu = (boton, overlay) => {
+    boton.addEventListener('click', () =>{
+        navegacion.classList.add('ocultar');
+        overlay.remove();
+        boton.remove();
+    });
+
+    overlay.onclick= function(){
+        overlay.remove();
+        navegacion.classList.add('ocultar');
+        boton.remove();
+    }
+}
